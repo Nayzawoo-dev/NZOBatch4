7f5b82d3b44ab636377eefca1e080b3d75d979c6
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-11-24 20:01:10 +0630

    feat: changenamespace

--- SqlServices/AdoSqlServices.cs
+++ SqlServices/AdoSqlServices.cs
@@ -3,53 +3,53 @@ using Newtonsoft.Json;
 using System.Data;
 using System.Security.Cryptography.X509Certificates;
 
-namespace SqlServices
+
+namespace NZOB4Database.Shared;
+
+public class AdoSqlServices
 {
-    public class AdoSqlServices
+    private readonly SqlConnectionStringBuilder _connection;
+    public AdoSqlServices(SqlConnectionStringBuilder connection)
     {
-        private readonly SqlConnectionStringBuilder _connection;
-        public AdoSqlServices(SqlConnectionStringBuilder connection)
-        {
-            _connection = connection;
-        }
+        _connection = connection;
+    }
 
-        public DataTable Query(string query, List<SqlParameter> parameters = null!)
-        {
-            SqlConnection connection = new SqlConnection(_connection.ConnectionString);
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(query, connection);
-            cmd.Parameters.AddRange(parameters.ToArray());
-            SqlDataAdapter adt = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adt.Fill(dt);
-            connection.Close();
-            return dt;
-        }
+    public DataTable Query(string query, List<SqlParameter> parameters = null!)
+    {
+        SqlConnection connection = new SqlConnection(_connection.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddRange(parameters.ToArray());
+        SqlDataAdapter adt = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adt.Fill(dt);
+        connection.Close();
+        return dt;
+    }
 
-        public int Execute(string query, params SqlParameter[] parameters)
-        {
-            SqlConnection connection = new SqlConnection(_connection.ConnectionString);
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(query,connection);
-            cmd.Parameters.AddRange(parameters);
-            int res = cmd.ExecuteNonQuery();
-            connection.Close();
-            return res;
-        }
+    public int Execute(string query, params SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(_connection.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query,connection);
+        cmd.Parameters.AddRange(parameters);
+        int res = cmd.ExecuteNonQuery();
+        connection.Close();
+        return res;
+    }
 
-        public List<T> Query<T>(string query, params SqlParameter[] parameters)
-        {
-            SqlConnection conn = new SqlConnection(_connection.ConnectionString);   
-            conn.Open();
-            SqlCommand cmd = new SqlCommand(query,conn);
-            cmd.Parameters.AddRange(parameters);
-            SqlDataAdapter adt = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adt.Fill(dt);
-            conn.Close();
-            string json = JsonConvert.SerializeObject(dt);
-            var res = JsonConvert.DeserializeObject<List<T>>(json)!;
-            return res;
-        }
+    public List<T> Query<T>(string query, params SqlParameter[] parameters)
+    {
+        SqlConnection conn = new SqlConnection(_connection.ConnectionString);   
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(query,conn);
+        cmd.Parameters.AddRange(parameters);
+        SqlDataAdapter adt = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adt.Fill(dt);
+        conn.Close();
+        string json = JsonConvert.SerializeObject(dt);
+        var res = JsonConvert.DeserializeObject<List<T>>(json)!;
+        return res;
     }
 }
--- SqlServices/DapperServices.cs
+++ SqlServices/DapperServices.cs
@@ -7,31 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NZOB4Database.Shared
-{
-    public class DapperServices
-    {
-        private readonly SqlConnectionStringBuilder _connection;
-        public DapperServices(SqlConnectionStringBuilder connection)
-        {
-            _connection = connection;
-        }
+namespace NZOB4Database.Shared;
 
-        public List<T> Query<T>(string query, object? parameters)
-        {
-            SqlConnection connection = new SqlConnection(_connection.ConnectionString);
-            connection.Open();
-            var res = connection.Query<T>(query, parameters).ToList();
-            connection.Close();
-            return res;
-        }
-        public int Execute(string query, object? parameters) 
-        { 
-            SqlConnection connection = new SqlConnection(_connection.ConnectionString);  
-            connection.Open();
-            var res = connection.Execute(query, parameters);
-            connection.Close();
-            return res;
-        }
+public class DapperServices
+{
+    private readonly SqlConnectionStringBuilder _connection;
+    public DapperServices(SqlConnectionStringBuilder connection)
+    {
+        _connection = connection;
+    }
+
+    public List<T> Query<T>(string query, object? parameters)
+    {
+        SqlConnection connection = new SqlConnection(_connection.ConnectionString);
+        connection.Open();
+        var res = connection.Query<T>(query, parameters).ToList();
+        connection.Close();
+        return res;
+    }
+    public int Execute(string query, object? parameters) 
+    { 
+        SqlConnection connection = new SqlConnection(_connection.ConnectionString);  
+        connection.Open();
+        var res = connection.Execute(query, parameters);
+        connection.Close();
+        return res;
     }
 }
