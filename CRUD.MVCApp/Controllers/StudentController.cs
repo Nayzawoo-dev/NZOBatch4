936bad56f58be8076a390c3fa43bb37e1a7507c0
Nay Zaw Oo <nayzawoo.ace@gmail.com>
2026-06-21 21:05:45 +0630

    feat: addFeatures

---
+++ CRUD.MVCApp/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace CRUD.MVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        [ActionName("Index")]
+        public async Task<IActionResult> StudentIndex()
+        {
+            SqlConnectionStringBuilder connectionstring = new SqlConnectionStringBuilder() { 
+            DataSource = "DELL",
+            InitialCatalog = "Revision",
+            UserID = "sa",
+            Password = "root",
+            TrustServerCertificate = true
+            };
+
+            using IDbConnection db = new SqlConnection(connectionstring.ConnectionString);
+            db.Open();
+            var lst =await db.QueryAsync<WalletModel>("select * from Students");
+            return View("StudentIndex", lst.ToList());
+        }
+    }
+
+    public class WalletModel { 
+        public string Roll_No { get; set; }
+        public string Name { get; set; }
+    }
+
+}
