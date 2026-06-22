986818aefac9e19eca6d8f4579586bd06521cb98
Nay Zaw Oo <nayzawoo.ace@gmail.com>
2026-06-22 23:13:38 +0630

    feat: AddFeat

--- CRUD.MVCApp/Controllers/StudentController.cs
+++ CRUD.MVCApp/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Data;
@@ -51,21 +52,63 @@ namespace CRUD.MVCApp.Controllers
             TempData["isSuccess"] = isSuccess;
             TempData["message"] = message;
 
-            var task1 = Run();
-            var task2 = Run();
-            await Task.WhenAll(task1, task2);
+            //var task1 = Run();
+            //var task2 = Run();
+            //await Task.WhenAll(task1, task2);
 
             return RedirectToAction("Index");
         }
 
-        public async Task Run()
-        {
+        //public async Task Run()
+        //{
 
+        //}
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> StudentEdit(int Id)
+        {
+            using IDbConnection db = new SqlConnection(connectionstring.ConnectionString);
+            db.Open();
+            string query = @"SELECT [Id]
+      ,[Roll_No]
+      ,[Name]
+      ,[Age]
+  FROM [dbo].[Students]
+  Where Id=@Id";
+            var lst = await db.QueryFirstOrDefaultAsync<StudentModel>(query, new StudentModel { Id = Id });
+            if (lst is null)
+            {
+                TempData["isSuccess"] = false;
+                TempData["message"] = "No Data Found";
+                return RedirectToAction("Index");
+            }
+            return View("EditIndex", lst);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> StudentUpdate(StudentModel responseModel)
+        {
+            using IDbConnection db = new SqlConnection(connectionstring.ConnectionString);
+            db.Open();
+            string query = @"UPDATE [dbo].[Students]
+   SET [Roll_No] = @Roll_No
+      ,[Name] = @Name
+ WHERE Id = @Id";
+            var lst = await db.ExecuteAsync(query, responseModel);
+            bool isSuccess = lst > 0;
+            string message = isSuccess ? "Success" : "Failed";
+            TempData["isSuccess"] = isSuccess;
+            TempData["message"] = message;
+
+            return RedirectToAction("Index");
         }
     }
 
     public class StudentModel
     {
+        public int Id { get; set; }
         public string Roll_No { get; set; }
         public string Name { get; set; }
     }
