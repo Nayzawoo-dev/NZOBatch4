0e685b067f24d54a7d8c432d1c943ba1c84195ea
Nay Zaw Oo <nayzawoo.ace@gmail.com>
2026-06-30 20:00:12 +0630

    feat: addRealData

--- AjaxApp.MVC/Controllers/ApexChartController.cs
+++ AjaxApp.MVC/Controllers/ApexChartController.cs
@@ -1,31 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AjaxApp.MVC.Controllers;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace SLHDotNetTrainingBatch1.MvcApp3.Controllers
 {
     public class ApexChartController : Controller
     {
-        public IActionResult PieChart()
+        private readonly SqlConnectionStringBuilder connectionstring;
+
+        public ApexChartController(IConfiguration configuration)
         {
-            ApexChartPieChartViewModel model = new ApexChartPieChartViewModel
+            connectionstring = new SqlConnectionStringBuilder(configuration.GetConnectionString("DbConnection"));
+        }
+
+        public async Task<IActionResult> PieChart()
+        {
+            using IDbConnection db = new SqlConnection(connectionstring.ConnectionString);
+
+            // 1. Query as dynamic to grab the raw database rows easily
+            var rawData = await db.QueryAsync("select Age, Name from Students");
+
+            // 2. Map the dynamic rows into your existing ViewModel's lists
+            var model = new ApexChartPieChartViewModel
             {
-                Series = new List<int> { 44, 55, 13, 43, 22 },
-                Labels = new List<string>
-                {
-                    "Team A",
-                    "Team B",
-                    "Team C",
-                    "Team D",
-                    "Team E"
-                }
+                Age = rawData.Select(x => (int)x.Age).ToList(),
+                Labels = rawData.Select(x => (string)x.Name).ToList()
             };
 
+            // 3. Pass your populated view model to the View
             return View(model);
         }
     }
 
     public class ApexChartPieChartViewModel
     {
-        public List<int> Series { get; set; }
+        public List<int> Age { get; set; }
 
         public List<string> Labels { get; set; }
     }
