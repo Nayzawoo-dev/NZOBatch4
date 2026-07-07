c24569eff65dcb971da1825bcaafd49274ad1993
Nay Zaw Oo <nayzawoo.ace@gmail.com>
2026-07-07 20:40:54 +0630

    feat: addFeat

---
+++ SeriLogExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Error()
+            .WriteTo.Console()
+            .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Hour)
+             .WriteTo
+    .MSSqlServer(
+        connectionString: "Server=DELL;Database=Revision;User ID=sa;Password=root;TrustServerCertificate=true;",
+        sinkOptions: new MSSqlServerSinkOptions { TableName = "Table_LogEvents" , AutoCreateSqlTable = true })
+            .CreateLogger();
+
+Log.Information("Hello, world!");
+
+int a = 10, b = 0;
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
