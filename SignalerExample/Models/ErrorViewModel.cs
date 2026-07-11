0ba66bc45583cbaca6fd0d87c22adddd34273055
Nay Zaw Oo <nayzawoo.ace@gmail.com>
2026-07-11 10:04:27 +0630

    feat: addSignaleR error

---
+++ SignalerExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalerExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
